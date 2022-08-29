1341160adf030be53fc35ade74b961f1d8119946
Tim Marquez <40231621+Relativity6@users.noreply.github.com>
2022-08-28 20:38:08 -0700

    Add files via upload

---
+++ Project Files/src/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+define('APP_ROOT', dirname(__FILE__, 2));                // Root directory
+
+require APP_ROOT . '/src/functions.php';
+require APP_ROOT . '/config/config.php';
+require APP_ROOT . '/vendor/autoload.php';
+
+if (DEV === false) {
+    set_exception_handler('handle_exception');           // Set exception handler
+    set_error_handler('handle_error');                   // Set error handler
+    register_shutdown_function('handle_shutdown');       // Set shutdown handler
+}
+
+spl_autoload_register(function($class)                   // Set autoload function
+{
+    $path = APP_ROOT . '/src/classes/';                  // Path to class definitions
+    require $path . $class . '.php';                     // Include class definition
+});
+
