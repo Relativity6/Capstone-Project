5dda48e262d9b2eb8940549c6b19daf04c111cf0
Tim Marquez <40231621+Relativity6@users.noreply.github.com>
2022-08-28 20:37:31 -0700

    Add files via upload

---
+++ Project Files/config/config.php
@@ -0,0 +1,27 @@
+<?php
+define('DEV', true);                                                                  // Development = true | Live = false
+define('DOMAIN', 'http://localhost:8888'); 
+
+// Folder Constants
+$this_folder   = substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT']));                  // Folder this file is in
+$parent_folder = dirname($this_folder);                                               // Parent of this folder
+
+
+define("DOC_ROOT", $parent_folder . '/public/');                                      // Document root (Capstone/public/)
+
+// Database settings
+$type = 'mysql';
+$server = 'localhost';
+$db = 'luminhealth';
+$port = '3306';
+$charset = 'utf8mb4';
+$username = 'user';
+$password = 'IFT402_Capstone';
+
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
+
+// File upload settings
+define('UPLOADS', DOC_ROOT . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR); //Image upload folder
+define('MEDIA_TYPES', ['image/jpeg', 'image/png', 'image/gif',]);                    // Allowed file types
+define('FILE_EXTENSIONS', ['jpeg', 'jpg', 'png', 'gif',]);                           // Allowed file extensions
+define('MAX_SIZE', '5242880');                                                       // Max file size
