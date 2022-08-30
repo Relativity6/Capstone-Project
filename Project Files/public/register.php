bc1c74e9b689393810028ec2c3c37a1bc5b48bc1
Tim Marquez <40231621+Relativity6@users.noreply.github.com>
2022-08-29 18:44:32 -0700

    Update register.php

--- Project Files/public/register.php
+++ Project Files/public/register.php
@@ -1,10 +1,8 @@
 <?php
 declare(strict_types = 1);
-// use Capstone\Validate\Validate;
-
 include '../src/bootstrap.php';
 
-// Initialize arrays
+// Initialize variables
 $member = [
     'fname' => '',
     'lname' => '',
@@ -13,16 +11,21 @@ $member = [
     'profile_pic' => '/img/Default_Profile_Pic.jpg',
     'phone_num' => ''
 ];
+
 $errors = [
     'fname' => '',
     'lname' => '',
     'email' => '',
     'password' => '',
     'confirm' => '',
+    'profile_pic' => '',
     'phone_num' => ''
 ];
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                     // If form was submitted
+$destination = '';
+$saved = null;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                                         // If form was submitted
 
     // Get form data
     $member['fname']        = $_POST['fname'];
@@ -48,17 +51,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                     
     $errors['phone_num'] = Validate::isPhoneNum($member['phone_num'])
         ? '' : 'Must be a valid 11 digit phone number';
 
-    $invalid = implode($errors);                                            // Join error messages
+    // Picture upload
+    $temp = $_FILES['profile_pic']['tmp_name'];                                                     // Temporary uploaded file name
 
-    if (!$invalid) {                                                        // If no errors
-        $result = $cms->getMember()->create($member);                       // Create member
+    $errors['profile_pic'] = ($_FILES['profile_pic']['error'] === 1) ? 'File too big' : '';         // If file too big
+    
+    if (is_uploaded_file($temp) and $_FILES['profile_pic']['error'] == 0) {                         // If a file was uploaded and there was no error in $_FILES superglobal array
 
-        if ($result === false) {                                            // If result is false, email is already in use
+        // Validate file type and size
+        $errors['profile_pic'] = in_array(mime_content_type($temp), MEDIA_TYPES) ? '' : 'Wrong file type';
+        $extension = strtolower(pathinfo($_FILES['profile_pic']['name'], PATHINFO_EXTENSION));
+        $errors['profile_pic'] .= in_array($extension, FILE_EXTENSIONS) ? '' : 'Wrong file extension. ';
+        $errors['profile_pic'] .= ($_FILES['profile_pic']['size'] <= MAX_SIZE) ? '' : 'File too big. ';
+        
+        // Create a destination file name and save to uploads folder
+        if ($errors['profile_pic'] === '') {
+            $member['profile_pic'] = create_filename($_FILES['profile_pic']['name'], 'uploads/');
+            $destination = 'uploads/' . $member['profile_pic'];                                     //Navigate to uploads from this file
+            move_uploaded_file($temp, $destination);
+        }
+
+    }
+
+    $invalid = implode($errors);                                                                    // Join error messages
+
+    if (!$invalid) {                                                                                // If no errors
+        $result = $cms->getMember()->create($member, $temp, $destination);                          // Create member
+
+        if ($result === false) {                                                                    // If result is false, email is already in use
             $errors['email'] = 'Email address already used';
         }
         
-        else {                                                              // If successful, redirect to login
-            redirect('login.php', ['success' => 'Thanks for joining! Please log in.']);        
+        else {                                                                                      // If successful, redirect to login
+            // Redirect to login.php is commented out until that page is completed
+            // redirect('login.php', ['success' => 'Thanks for joining! Please log in.']);        
         }
     }
 }
@@ -75,7 +101,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                     
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
-        <link rel = 'stylesheet' href = 'css/styles_register.css'>
+        <link rel = 'stylesheet' href = 'css/register.css'>
     </head>
     <body>
         <div id = 'container'>
@@ -87,67 +113,79 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                     
                 <h1>Create your free account</h1>
                 <h3>Get started by entering your information below</h3>
 
-                <form action = 'register.php' method = 'POST' enctype = 'multipart/form-data' accept = 'image/*'>
-                    <!-- First name -->
-                    <label for = 'fname'>First name:</label>
-                    <input type = 'text' name = 'fname'>
-                    <?php if ($errors['fname']) {?>
-                            <p class = 'warning'>
-                                <?= $errors['fname'] ?>
-                            </p>
-                    <?php } ?>
-                    
-                    <!-- Last Name -->
-                    <label for = 'lname'>Last name:</label>
-                    <input type = 'text' name = 'lname'>
-                    <?php if ($errors['lname']) {?>
-                            <p class = 'warning'>
-                                <?= $errors['lname'] ?>
-                            </p>
-                    <?php } ?>
 
-                    <!-- Email -->
-                    <label for = 'email'>Email:</label>
-                    <input type = 'email' name = 'email'>
-                    <?php if ($errors['email']) {?>
-                            <p class = 'warning'>
-                                <?= $errors['email'] ?>
-                            </p>
-                    <?php } ?>
+                <div id = 'form_div'>
+                    <form action = 'register.php' method = 'POST' enctype = 'multipart/form-data' accept = 'image/*'>
 
-                    <!-- Password -->
-                    <label for = 'password'>Password:</label>
-                    <input type = 'password' name = 'password'>
-                    <?php if ($errors['password']) {?>
-                            <p class = 'warning'>
-                                <?= $errors['password'] ?>
-                            </p>
-                    <?php } ?>
+                        <?php if (implode($errors)) { ?>
 
-                    <!-- Confirm password -->
-                    <label for = 'confirm'>Confirm password:</label>
-                    <input type = 'password' name = 'confirm'>
-                    <?php if ($errors['confirm']) {?>
-                            <p class = 'warning'>
-                                <?= $errors['confirm'] ?>
-                            </p>
-                    <?php } ?>
+                            <h3 class = 'warning'>
+                                Please correct the errors below.
+                            </h3>
 
-                    <!-- Phone number -->
-                    <label for = 'phone_num'>Phone number (Format: X-XXX-XXX-XXXX):</label>
-                    <input type = 'tel' name = 'phone_num'  pattern = '[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}' id = 'phone_num'>
-                    <?php if ($errors['phone_num']) {?>
-                            <p class = 'warning'>
-                                <?= $errors['phone_num'] ?>
-                            </p>
-                    <?php } ?>
+                        <?php } ?>
 
-                    <!-- Profile picture -->
-                    <label for = 'profile_pic'>Profile picture:</label>
-                    <input type = 'file' name = 'profile_pic'>
+                        <!-- First name -->
+                        <label for = 'fname'>First name:</label>
+                        <input type = 'text' name = 'fname'>
+                        <?php if ($errors['fname']) {?>
+                                <p class = 'warning'>
+                                    <?= $errors['fname'] ?>
+                                </p>
+                        <?php } ?>
+                        
+                        <!-- Last Name -->
+                        <label for = 'lname'>Last name:</label>
+                        <input type = 'text' name = 'lname'>
+                        <?php if ($errors['lname']) {?>
+                                <p class = 'warning'>
+                                    <?= $errors['lname'] ?>
+                                </p>
+                        <?php } ?>
 
-                    <input type = 'submit' id = 'submit'>
-                </form>
+                        <!-- Email -->
+                        <label for = 'email'>Email:</label>
+                        <input type = 'email' name = 'email'>
+                        <?php if ($errors['email']) {?>
+                                <p class = 'warning'>
+                                    <?= $errors['email'] ?>
+                                </p>
+                        <?php } ?>
+
+                        <!-- Password -->
+                        <label for = 'password'>Password:</label>
+                        <input type = 'password' name = 'password'>
+                        <?php if ($errors['password']) {?>
+                                <p class = 'warning'>
+                                    <?= $errors['password'] ?>
+                                </p>
+                        <?php } ?>
+
+                        <!-- Confirm password -->
+                        <label for = 'confirm'>Confirm password:</label>
+                        <input type = 'password' name = 'confirm'>
+                        <?php if ($errors['confirm']) {?>
+                                <p class = 'warning'>
+                                    <?= $errors['confirm'] ?>
+                                </p>
+                        <?php } ?>
+
+                        <!-- Phone number -->
+                        <label for = 'phone_num'>Phone number (Format: X-XXX-XXX-XXXX):</label>
+                        <input type = 'tel' name = 'phone_num'  pattern = '[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}' id = 'phone_num'>
+                        <?php if ($errors['phone_num']) {?>
+                                <p class = 'warning'>
+                                    <?= $errors['phone_num'] ?>
+                                </p>
+                        <?php } ?>
+
+                        <!-- Profile picture -->
+                        <label for = 'profile_pic'>Profile picture:</label>
+                        <input type = 'file' name = 'profile_pic'>
+
+                        <input type = 'submit' id = 'submit'>
+                    </form>
+                </div>
             </div>
         </div>
     </body>
