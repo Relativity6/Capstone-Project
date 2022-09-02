dc6a21d4d8263a7020860cfced112562b93a561d
Tim Marquez <40231621+Relativity6@users.noreply.github.com>
2022-09-01 19:57:07 -0700

    Update bootstrap.php

--- Project Files/src/bootstrap.php
+++ Project Files/src/bootstrap.php
@@ -3,7 +3,7 @@ define('APP_ROOT', dirname(__FILE__, 2));                // Root directory
 
 require APP_ROOT . '/src/functions.php';
 require APP_ROOT . '/config/config.php';
-require APP_ROOT . '/vendor/autoload.php';
+// require APP_ROOT . '/vendor/autoload.php';
 
 if (DEV === false) {
     set_exception_handler('handle_exception');           // Set exception handler
