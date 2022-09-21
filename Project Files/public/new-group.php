5129ff03624f11fb4275154aacc90354b59823c0
Tim Marquez <40231621+Relativity6@users.noreply.github.com>
2022-09-21 16:11:22 -0700

    Update new-group.php

--- Project Files/public/new-group.php
+++ Project Files/public/new-group.php
@@ -2,64 +2,78 @@
 declare(strict_types = 1);
 include '../src/bootstrap.php';
 
+// Redirect checks
+$id = (int) $_SESSION['id'];
+if (!$id) {
+    redirect('login.php');
+}
+
+$member = $cms->getMember()->get($id);
+if (!$member) {
+    redirect('page-not-found.php');
+}
+
 // Initialize variables
 $group = [
-    'gname' => '',
-    'password' => '',
-    'admin_id' => ''
+    'name'       => '',
+    'password'   => '',
+    'admin_id'   => ''
 ];
 
 $errors = [
-    'gname' => '',
-    'password' => '',
-    'confirm' => '',
-    'admin_id' => ''
+    'name'       => '',
+    'password'   => '',
+    'confirm'    => ''
 ];
 
-$destination = '';
-$saved = null;
+$membership_args = [
+    'user_id'    => '',
+    'group_id'   => '',
+    'role'       => ''
+];
 
-$id = (int) $_SESSION['id'];                                // Get id from $_SESSION
-if (!$id) {                                                 // If no id in $_SESSION, user not logged in
-    redirect('login.php');                                  // Redirect to login.php
-}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-$member = $cms->getMember()->get($id);                      // Find user with given id
-if (!$member) {                                             // If user not found in DB
-    redirect('page-not-found.php');                         // Redirect to page-not-found.php
-}
-else {
-    $group['admin_id'] = $id;
-}
+    // Get form data and populate Group array
+    $group['name']          = $_POST['name'];
+    $group['password']      = $_POST['password'];
+    $confirm                = $_POST['confirm'];
+    $group['admin_id']      = $id;
 
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                                         // If form was submitted
-
-    // Get form data
-    $group['gname']        = $_POST['gname'];
-    $group['password']     = $_POST['password'];
-    $confirm_password       = $_POST['confirm'];
-
-    // Validate form data
-    $errors['gname']     = Validate::isText($group['gname'], 1, 20)
+    // Error validation
+    $errors['name']      = Validate::isText($group['name'], 1, 20)
         ? '' : 'Name must be 1-20 characters';
     $errors['password']  = Validate::isPassword($group['password'])
         ? '' : 'Passwords must be at least 8 characters and have:<br> 
                 A lowercase letter<br>An uppercase letter<br>A number 
                 <br>And a special character';
+    $errors['confirm']   = ($group['password'] == $confirm) ? '' : 'Passwords do not match';
 
-    $invalid = implode($errors);                                                                    // Join error messages
+    $invalid = implode($errors);
 
-    if (!$invalid) {                                                                                // If no errors
-        $result = $cms->getGroup()->create($group, $destination);                                   // Create group
+    // If no errors, call Create function
+    if (!$invalid) {
+        $result = $cms->getGroup()->create($group);
 
-        if ($result === false) {                                                                    // If result is false, name is already in use
-            $errors['gname'] = 'Name already used';
+        if ($result == false) {
+            $errors['name'] = 'Name already in use.';
         }
-        
-// Change redirect *********************************************************************************
-        else {                                                                                      // If successful, redirect to login
-            redirect('group.php', ['success' => 'Thanks for joining! Please log in.']);        
+
+        // If Group Create function successful, use "getByGroupName" function to retrieve group ID
+        // and create a new Membership instance with user_id and group_id with the role of "admin"
+        else {
+            $group_info = $cms->getGroup()->getByGroupName($group['name']);
+
+            $membership_args['user_id']    = $id;
+            $membership_args['group_id']   = $group_info['id'];
+            $membership_args['role']       = 'admin';
+            
+            $membership_result = $cms->getMembership()->joinGroup($membership_args);
+
+            // If no errors, redirect to group.php with the group_id in the query string
+            if ($membership_result) {
+                redirect('group.php', ['group_id' => $group_info['id']]);
+            }
         }
     }
 }
@@ -69,44 +83,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                     
 <html lang = 'en'>
     <head>
         <title>
-            LuminHealth | Create a group
+            LuminHealth | New Group
         </title>
         <meta charset = 'utf-8'>
         <meta name = 'description' content = ''>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
-        <link rel = 'stylesheet' href = 'css/create-group.css'>
+        <link rel = 'stylesheet' href = 'css/new-group.css'>
     </head>
     <body>
         <div id = 'container'>
-<!-- Change image **************************************************************************************** -->
-            <img id = 'hero_image' src = 'img/jed-villejo-pumko2FFxY0-unsplash.jpg' alt = ''/>
+            <img id = 'hero_image' src = '' alt = ''/>
 
             <div id = 'content'>
                 <img id = 'logo' src = 'img/logo2_transparent.png' alt = 'LuminHealth logo'>
 
                 <h1>Create a group</h1>
-                <h3>Get started by entering your information below</h3>
-
 
                 <div id = 'form_div'>
-                    <form action = 'new-group.php' method = 'POST' enctype = 'multipart/form-data'>
+                    <form action = 'new-group.php' method = 'POST'>
 
+                        <!-- Error prompt -->
                         <?php if (implode($errors)) { ?>
-
                             <h3 class = 'warning'>
                                 Please correct the errors below.
                             </h3>
-
                         <?php } ?>
 
                         <!-- Group name -->
-                        <label for = 'gname'>Group name:</label>
-                        <input type = 'text' name = 'gname'>
-                        <?php if ($errors['gname']) {?>
+                        <label for = 'name'>Group name:</label>
+                        <input type = 'text' name = 'name'>
+                        <?php if ($errors['name']) {?>
                                 <p class = 'warning'>
-                                    <?= $errors['gname'] ?>
+                                    <?= $errors['name'] ?>
                                 </p>
                         <?php } ?>
 
@@ -128,11 +138,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {                                     
                                 </p>
                         <?php } ?>
 
-
-                        <input type = 'submit' id = 'submit'>
+                        <input type = 'submit' id = 'create_button' value = 'Create group'>
                     </form>
-
-                    </p>
                 </div>
             </div>
         </div>
